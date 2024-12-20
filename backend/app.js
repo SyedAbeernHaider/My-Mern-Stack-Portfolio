b236c7258ec691b9ab845d6fe330d2866201d78b
Syed Abeer Bin Haider <mcareer259@gmail.com>
2024-12-20 17:17:22 +0500

    orignal code

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,27 @@
 const express = require("express");
-const path = require("path");
 require("dotenv").config(); // Load environment variables
 require("./connection/connect"); // Connect to MongoDB
 const auth = require("./Routes/auth.js");
 const cors = require("cors");
+const path = require("path");
+
 
 const app = express();
-const PORT = process.env.PORT || 1500;
+const PORT = process.env.PORT || 9500;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
-// Serve static files from the public folder
-app.use(express.static(path.join(__dirname, "frontend", "public")));
-
-// Serve static files from the React build folder
-app.use(express.static(path.join(__dirname, "frontend", "build")));
-
-// API Routes
+// Routes
 app.use("/user", auth);
-
-// Serve React app index.html for any other route
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
+
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`SERVER STARTED AT PORT ${PORT}`);
 });
-
-
